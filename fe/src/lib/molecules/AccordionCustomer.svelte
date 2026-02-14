0f3ce898b039658e51cea7f4204173805267d82f
DarilInsanKamil <darilinsankamil2132@gmail.com>
2026-02-14 21:43:20 +0700

    feat: berhasil create reservasi (walaupun beberapa data di hardcode)

--- fe/src/lib/molecules/AccordionCustomer.svelte
+++ fe/src/lib/molecules/AccordionCustomer.svelte
@@ -1,17 +1,45 @@
 <script lang="ts">
+    import { enhance } from "$app/forms";
  import * as Accordion from "$lib/components/ui/accordion/index.js";
     import Button from "$lib/components/ui/button/button.svelte";
+    import { toast } from "svelte-sonner";
 
- let { form = $bindable()} = $props<{
-     form: any;
- }>();
+    let {
+            form,
+            armadaId,
+            tglMulai,
+            tglSelesai,
+            totalHarga
+        } = $props<{
+            form: any;
+            armadaId: string;
+            tglMulai: string;
+            tglSelesai: string;
+            totalHarga: string;
+        }>();
+
 </script>
 
 <Accordion.Root type="single" class="w-full bg-white rounded-sm px-2 shadow-sm" value="item-1">
  <Accordion.Item value="item-1">
   <Accordion.Trigger>Isi Data Reservasi</Accordion.Trigger>
   <Accordion.Content class="flex flex-col gap-4 text-balance">
-      <form action="?reservasi" method="POST" class="flex flex-col gap-3">
+      <form action="?reservasi" method="POST" class="flex flex-col gap-3" use:enhance={() => {
+        return async({result, update}) => {
+          if(result.type === 'success') {
+            toast.success('Berhasil Reservasi, Silakan lanjutkan pembayaran')
+          } else if (result.type === 'failure') {
+            toast.error((result.data as { message?: string })?.message || "Terjadi kesalahan")
+          }
+          await update()
+        }
+      }}>
+          <input type="hidden" name="armada_id" value={armadaId} />
+                  <input type="hidden" name="tgl_mulai" value={tglMulai} />
+                  <input type="hidden" name="tgl_selesai" value={tglSelesai} />
+
+                  <input type="hidden" name="total_harga" value={totalHarga} />
+
           <div>
               <label for="nama_lengkap" class="font-semibold">
                   Nama Lengkap *
@@ -68,7 +96,7 @@
                   required
               >
           </div>
-          <Button>Lanjut ke Pembayaran</Button>
+          <Button type='submit'>Lanjut ke Pembayaran</Button>
       </form>
   </Accordion.Content>
  </Accordion.Item>
